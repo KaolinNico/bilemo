97b5b5f07747a9b5ab7ffcc6e284ef13065fe6ad
nicolas <nicolas.signolet@gmail.com>
2020-07-21 20:21:24 +0200

    add list/show/edit/delete

--- src/Form/PhoneType.php
+++ src/Form/PhoneType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Phone;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,19 +13,19 @@ class PhoneType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('reference')
-            ->add('brand')
-            ->add('model')
-            ->add('price')
-            ->add('processor')
-            ->add('screen')
-            ->add('camera')
-            ->add('ram')
-            ->add('network')
-            ->add('connectivity')
-            ->add('system')
-            ->add('autonomy')
-            ->add('dimensions')
+            ->add('reference', TextType::class)
+            ->add('brand', TextType::class)
+            ->add('model', TextType::class)
+            ->add('price', TextType::class)
+            ->add('processor', TextType::class)
+            ->add('screen', TextType::class)
+            ->add('camera', TextType::class)
+            ->add('ram', TextType::class)
+            ->add('network', TextType::class)
+            ->add('connectivity', TextType::class)
+            ->add('system', TextType::class)
+            ->add('autonomy', TextType::class)
+            ->add('dimensions', TextType::class)
         ;
     }
 
